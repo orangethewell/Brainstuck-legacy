b74497349850b6802452f4fc386a8b52278ecc88
orangethewell <sempjunior05@gmail.com>
2021-07-08 21:19:21 -0300

    Move files to `./src`

--- input.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-char* in__get_all_text(FILE *filep);
-
--- interpreter.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define STCK_SIZE 1024
-#define CHAR_LIMIT 255
-
-int bf__exec_instruction(char instruction_i);
-void bf__conditional_loop(int expr_buffer_position, int jump);
-void bf__main_loop(char* code);
